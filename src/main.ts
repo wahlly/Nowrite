33b112a4df7c2a7a3b43849156ec9eebb4c5ea1c
wahllyWest <olaifaolawaleh@gmail.com>
2023-11-25 22:32:58 +0100

    swagger docs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //swagger docs
   const config = new DocumentBuilder()
     .setTitle("nowrite")
     .setDescription('api distribution')
